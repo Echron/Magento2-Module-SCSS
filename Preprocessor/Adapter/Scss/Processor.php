a0bdd505f3eef3f40fce92923484ac473af87dad
Stijn Duynslaeger <info@echron.eu>
2016-12-17 03:08:14 +0100

    Cleanup Update composer.json

--- Preprocessor/Adapter/Scss/Processor.php
+++ Preprocessor/Adapter/Scss/Processor.php
@@ -3,6 +3,7 @@
 namespace Echron\Scss\Preprocessor\Adapter\Scss;
 
 use Leafo\ScssPhp\Compiler;
+use Leafo\ScssPhp\Formatter;
 use Magento\Framework\App\State;
 use Magento\Framework\Phrase;
 use Magento\Framework\View\Asset\ContentProcessorException;
@@ -17,11 +18,11 @@ use Psr\Log\LoggerInterface;
 class Processor implements ContentProcessorInterface
 {
     const FORMATTER_AUTO = 'auto';
-    const FORMATTER_EXPANDED = \Leafo\ScssPhp\Formatter\Expanded::class;
-    const FORMATTER_NESTED = \Leafo\ScssPhp\Formatter\Nested::class;
-    const FORMATTER_COMPRESSED = \Leafo\ScssPhp\Formatter\Compressed::class;
-    const FORMATTER_COMPACT = \Leafo\ScssPhp\Formatter\Compact::class;
-    const FORMATTER_CRUNCHED = \Leafo\ScssPhp\Formatter\Crunched::class;
+    const FORMATTER_EXPANDED = Formatter\Expanded::class;
+    const FORMATTER_NESTED = Formatter\Nested::class;
+    const FORMATTER_COMPRESSED = Formatter\Compressed::class;
+    const FORMATTER_COMPACT = Formatter\Compact::class;
+    const FORMATTER_CRUNCHED = Formatter\Crunched::class;
 
     const LINE_NUMBER_STYLE_AUTO = 'auto';
     const LINE_NUMBER_STYLE_ON = 'on';
@@ -35,6 +36,9 @@ class Processor implements ContentProcessorInterface
      */
     private $assetSource;
 
+    /**
+     * @var State
+     */
     private $appState;
 
     /**
@@ -42,8 +46,9 @@ class Processor implements ContentProcessorInterface
      *
      * @param Source $assetSource
      * @param LoggerInterface $logger
+     * @param State $appState
      */
-    public function __construct(Source $assetSource, LoggerInterface $logger, \Magento\Framework\App\State $appState)
+    public function __construct(Source $assetSource, LoggerInterface $logger, State $appState)
     {
         $this->assetSource = $assetSource;
         $this->logger = $logger;
@@ -51,17 +56,14 @@ class Processor implements ContentProcessorInterface
     }
 
     /**
-     * Process file content
-     *
-     * @param File $asset
-     * @return string
+     * @inheritdoc
+     * @throws ContentProcessorException
      */
     public function processContent(File $asset)
     {
         $path = $asset->getPath();
         try {
 
-            //  $compiler = new \scssc();
             $content = $this->assetSource->getContent($asset);
 
             if (trim($content) === '') {
@@ -70,54 +72,24 @@ class Processor implements ContentProcessorInterface
 
             $source = $this->assetSource->findSource($asset);
 
-//TODO: needed?
-            $styleFolder = realpath(dirname($source));
-            //If source ends with "web/css" then add styles folder
-            $styleFolder2 = str_replace('web/css', 'styles', $styleFolder);
             $compiler = $this->getSCSSCompiler();
 
-            $folders = [
-                $styleFolder,
-                $styleFolder2,
-                BP,
-            ];
+            $sourceDirectories = $this->getSourceDirectories($source);
+
             $compiler->setImportPaths([]);
-            $compiler->addImportPath(function ($path) use ($folders) {
-
-                $fileInfo = pathinfo($path);
-                $alternativePath = $fileInfo['dirname'] . '/_' . $fileInfo['basename'];
-
-                //TODO: remove sass
-                $files = [
-                    $path . '.scss',
-                    $path . '.sass',
-                    $alternativePath . '.scss',
-                    $alternativePath . '.sass',
-                ];
-                foreach ($files as $file) {
-                    foreach ($folders as $folder) {
-                        $filePath = $folder . '/' . $file;
-
-                        $fullPath = realpath($filePath);
-                        $exists = file_exists($fullPath);
-                        if ($exists) {
-                            return $fullPath;
-                        } else {
-                        }
-                    }
-                }
-
-                return null;
-
+            $compiler->addImportPath(function ($path) use ($sourceDirectories) {
+                return $this->findImportPath($path, $sourceDirectories);
             });
 
             $result = $compiler->compile($content);
-            $result = '/* Generated ' . date("Y-m-d H:i:s") . ' */' . PHP_EOL . $result;
+            if ($this->appState->getMode() === State::MODE_DEVELOPER) {
+                $result = '/* Generated ' . date("Y-m-d H:i:s") . ' */' . PHP_EOL . $result;
+                //TODO: add compile duration
+            }
 
             return $result;
         } catch (\Exception $e) {
 
-            die('Ex: ' . $e->getMessage());
             $errorMessage = PHP_EOL . self::ERROR_MESSAGE_PREFIX . PHP_EOL . $path . PHP_EOL . $e->getMessage();
             $this->logger->critical($errorMessage);
 
@@ -171,7 +143,7 @@ class Processor implements ContentProcessorInterface
     {
         $setting = $this->getFormattingConfig();
 
-        $formatting = self::FORMATTER_NESTED;
+        $formatting = null;
         switch ($setting) {
             case self::FORMATTER_AUTO:
                 if ($this->appState->getMode() === State::MODE_DEVELOPER) {
@@ -190,5 +162,48 @@ class Processor implements ContentProcessorInterface
     private function getFormattingConfig()
     {
         return self::FORMATTER_AUTO;
+    }
+
+    /**
+     * @param $source
+     * @return array
+     */
+    private function getSourceDirectories($source)
+    {
+        $styleFolder = realpath(dirname($source));
+        //If source ends with "web/css" then add styles folder
+        $styleFolder2 = str_replace('web/css', 'styles', $styleFolder);
+
+        $folders = [
+            $styleFolder,
+            $styleFolder2,
+            BP,
+        ];
+
+        return $folders;
+    }
+
+    private function findImportPath($path, $sourceDirectories)
+    {
+        $fileInfo = pathinfo($path);
+        $alternativePath = $fileInfo['dirname'] . DIRECTORY_SEPARATOR . '_' . $fileInfo['basename'];
+
+        $files = [
+            $path . '.scss',
+            $alternativePath . '.scss',
+        ];
+        foreach ($files as $file) {
+            foreach ($sourceDirectories as $sourceDirectory) {
+                $filePath = $sourceDirectory . DIRECTORY_SEPARATOR . $file;
+
+                $fullPath = realpath($filePath);
+                $exists = file_exists($fullPath);
+                if ($exists) {
+                    return $fullPath;
+                }
+            }
+        }
+
+        return null;
     }
 }
